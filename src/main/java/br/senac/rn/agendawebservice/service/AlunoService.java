0ee1883e3833f064fa6269406631d49a4f44a329
a9122116 <a9122116@02LAB108PC03.rn.senac.br>
2017-11-06 21:25:01 -0300

    gerenciador web

--- src/main/java/br/senac/rn/agendawebservice/service/AlunoService.java
+++ src/main/java/br/senac/rn/agendawebservice/service/AlunoService.java
@@ -3,7 +3,10 @@ package br.senac.rn.agendawebservice.service;
 import br.senac.rn.agendawebservice.dao.AlunoDao;
 import br.senac.rn.agendawebservice.model.Aluno;
 import java.util.List;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
@@ -15,11 +18,30 @@ public class AlunoService {
 
     private final AlunoDao dao = new AlunoDao();
     
-    public void salvar() {}
+    @POST
+    @Path("/salvar")
+    @Consumes({MediaType.APPLICATION_JSON})    
+    public void salvar(Aluno aluno) {
+    dao.insert(aluno);
+    }
     
-    public void editar() {}
     
-    public void excluir() {}
+//    @
+//    @Path("/editar/{matricula}")
+    public void editar(@PathParam("matricula") Integer matricula) {
+//        Aluno aluno = dao.selectByMatricula(matricula); 
+//        dao.delete(aluno);
+    }
+    
+    
+    
+    @DELETE
+    @Path("/remover/{matricula}")
+    public void excluir(@PathParam("matricula") Integer matricula) {    
+        Aluno aluno = dao.selectByMatricula(matricula);        
+        dao.delete(aluno);
+    }
+    
     
     @GET    
     @Path("/matricula/{matricula}")
